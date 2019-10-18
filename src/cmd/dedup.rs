168b50057872db4e0f9af75cbdfac2ba52718b9d
Nithin Philips <nithin@nithinphilips.com>
2019-10-18 09:35:27 -0400

    Fix compile issue in dedup

--- src/cmd/dedup.rs
+++ src/cmd/dedup.rs
@@ -31,7 +31,7 @@ Common options:
                            Must be a single character. (default: ,)
 ";
 
-#[derive(RustcDecodable)]
+#[derive(Deserialize)]
 struct Args {
     arg_input: Option<String>,
     flag_select: SelectColumns,
